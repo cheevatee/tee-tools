4f2aa0e6755b472109491ab6eeb41f1e2324a7ab
cheevatee <38664398+cheevatee@users.noreply.github.com>
2022-02-22 20:17:36 +0700

    Add current dir to index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
   <body>
     <p><?php echo "hostname is: ".gethostname(); ?></p>
+    <p><?php echo "current dir is: ".getcwd(); ?></p>
     <p><?php echo date("D M j G:i:s T Y"); ?></p>
   </body>
 </html>
